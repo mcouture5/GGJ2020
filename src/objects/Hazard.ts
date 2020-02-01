4f926a590cca7eff5c78673be978025e38ed926f
Mike <mikejcouture@gmail.com>
2020-01-31 22:11:29 -0500

    Layout

---
+++ src/objects/Hazard.ts
@@ -0,0 +1,12 @@
+export class Hazard extends Phaser.GameObjects.Sprite {
+    constructor(params) {
+        super(params.scene, params.x, params.y, params.key, params.frame);
+
+        // image
+        this.setScale(3);
+        this.setOrigin(0, 0);
+    }
+
+    update(): void {
+    }
+}
