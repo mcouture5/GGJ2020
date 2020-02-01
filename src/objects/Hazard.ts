323232251dbd2e5d6fd6dc40eca56c4dccb67079
jdelapp <josh.delapp@claritybizsol.com>
2020-02-01 09:44:43 -0500

    Hazard sizing is now set in layout.json

--- src/objects/Hazard.ts
+++ src/objects/Hazard.ts
@@ -3,6 +3,7 @@ import { IFubarObject, FubarObject } from "./FubarObject";
 export interface IHazard {
     key: string;
     position: { x: number, y: number };
+    display: { width: number, height: number };
     tool: string;
 }
 
@@ -13,8 +14,8 @@ export class Hazard extends FubarObject {
 
         // image
         this.setOrigin(0, 0);
-        this.displayWidth = 32;
-        this.displayHeight = 64;
+        this.displayWidth = hazard.display.width;
+        this.displayHeight = hazard.display.width;
     }
 
     update(): void {
