08b96f9b648ce128ceb7b25615aee7b130d1fe85
Mike <mikejcouture@gmail.com>
2020-02-01 12:36:47 -0500

    Added hazards

--- src/objects/Room.ts
+++ src/objects/Room.ts
@@ -1,4 +1,4 @@
-import { IHazard } from "./Hazard";
+import { IHazard, Hazard } from "./Hazard";
 import { FubarObject, IFubarObject } from "./FubarObject";
 import { HazardGroup } from "../groups/HazardGroup";
 import { IDoor } from "./Door";
@@ -25,12 +25,13 @@ export class Room extends Phaser.GameObjects.Container {
 
         this.doors = room.doors;
         this.hazards = room.hazards;
+        this.children = [];
 
         // Create background image
         this.add(new Phaser.GameObjects.Sprite(this.scene, 0, 0, 'room_bg').setOrigin(0.5, 0.5).setScale(0.5, 0.5));
 
         // Create hazards in the room
-        //this.hazardGroup = new HazardGroup({ scene: this.scene, hazards: room.hazards });
+        this.createHazards();
 
         // Create the doors
         //this.createDoors();
@@ -41,6 +42,14 @@ export class Room extends Phaser.GameObjects.Container {
 
     getDoors() {
         return this.doors;
+    }
+
+    private createHazards() {
+        for (let hazard of this.hazards) {
+            let hd = new Hazard({ scene: this.scene, x: hazard.position.x, y: hazard.position.y, key: hazard.key }, hazard);
+            this.add(hd);
+            this.children.push(hd);
+        }
     }
     
 }
