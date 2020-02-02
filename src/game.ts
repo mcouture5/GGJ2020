2f6e6df2bdb5b976d906881ecd01ce22950dc827
Shahrukh Mustakim <shahrukh@umd.edu>
2020-02-02 08:51:04 -0500

    Credits and not-instructions

--- src/game.ts
+++ src/game.ts
@@ -3,6 +3,8 @@
 import 'phaser';
 import { Boot } from './scenes/Boot';
 import { MainMenu } from './scenes/MainMenu';
+import { Credits } from './scenes/Credits';
+// import { Instructions } from './scenes/Instructions';
 import { GameScene } from './scenes/GameScene';
 import { LevelIntro } from './scenes/LevelIntro';
 import { HUDScene } from './scenes/HUDScene';
@@ -14,7 +16,7 @@ const config: GameConfig = {
   height: 768,
   type: Phaser.WEBGL,
   parent: 'game',
-  scene: [Boot, MainMenu, GameScene, LevelIntro, HUDScene],
+  scene: [Boot, MainMenu, /*Instructions, */Credits, GameScene, LevelIntro, HUDScene],
   input: {
     keyboard: true,
     mouse: false,
