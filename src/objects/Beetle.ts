acb45294c4ce68b1b64e506fc635fb608e6d0252
Shahrukh Mustakim <shahrukh@umd.edu>
2020-02-01 21:11:55 -0500

    tool actions/animations

--- src/objects/Beetle.ts
+++ src/objects/Beetle.ts
@@ -6,9 +6,14 @@ export class Beetle extends Phaser.GameObjects.Sprite {
 	protected rightKey: Phaser.Input.Keyboard.Key;
     protected enterDoorKey: Phaser.Input.Keyboard.Key;
     protected actionKey: Phaser.Input.Keyboard.Key;
+    protected hammerKey: Phaser.Input.Keyboard.Key;
+    protected plungerKey: Phaser.Input.Keyboard.Key;
+    protected screwdriverKey: Phaser.Input.Keyboard.Key;
+    protected wrenchKey: Phaser.Input.Keyboard.Key;
 
     protected beetleEvents: Phaser.Events.EventEmitter;
     protected moveSpeed: integer;
+    protected toolEquipped: integer; // 0=none,1=hammer,2=plunger,3=screwdriver,4=wrench
 
     protected roomCoords;
 
@@ -32,10 +37,59 @@ export class Beetle extends Phaser.GameObjects.Sprite {
             frameRate: 8,
             repeat: -1,
         });
+        params.scene.anims.create({
+            key: 'hammer',
+            frames: params.scene.anims.generateFrameNames('beetle', { start: 4, end: 4 }),
+            frameRate: 8,
+            repeat: -1,
+        });
+        params.scene.anims.create({
+            key: 'use-hammer',
+            frames: params.scene.anims.generateFrameNames('beetle', { start: 5, end: 6 }),
+            frameRate: 8,
+            repeat: 0,
+        });
+        params.scene.anims.create({
+            key: 'plunger',
+            frames: params.scene.anims.generateFrameNames('beetle', { start: 7, end: 7 }),
+            frameRate: 8,
+            repeat: -1,
+        });
+        params.scene.anims.create({
+            key: 'use-plunger',
+            frames: params.scene.anims.generateFrameNames('beetle', { start: 8, end: 9 }),
+            frameRate: 8,
+            repeat: 0,
+        });
+        params.scene.anims.create({
+            key: 'screwdriver',
+            frames: params.scene.anims.generateFrameNames('beetle', { start: 10, end: 10 }),
+            frameRate: 8,
+            repeat: -1,
+        });
+        params.scene.anims.create({
+            key: 'use-screwdriver',
+            frames: params.scene.anims.generateFrameNames('beetle', { start: 11, end: 12 }),
+            frameRate: 8,
+            repeat: 0,
+        });
+        params.scene.anims.create({
+            key: 'wrench',
+            frames: params.scene.anims.generateFrameNames('beetle', { start: 13, end: 13 }),
+            frameRate: 8,
+            repeat: -1,
+        });
+        params.scene.anims.create({
+            key: 'use-wrench',
+            frames: params.scene.anims.generateFrameNames('beetle', { start: 14, end: 15 }),
+            frameRate: 8,
+            repeat: 0,
+        });
+        this.toolEquipped = 0;
         this.anims.play('idle');
 
         // image
-        this.setScale(0.4);
+        this.setScale(0.25);
         this.setOrigin(0.5, 1);
         this.setDepth(800);
         this.moveSpeed = 200;
@@ -57,6 +111,18 @@ export class Beetle extends Phaser.GameObjects.Sprite {
         this.actionKey = params.scene.input.keyboard.addKey(
             Phaser.Input.Keyboard.KeyCodes.SPACE
         );
+        this.hammerKey = params.scene.input.keyboard.addKey(
+            Phaser.Input.Keyboard.KeyCodes.ONE
+        );
+        this.plungerKey = params.scene.input.keyboard.addKey(
+            Phaser.Input.Keyboard.KeyCodes.TWO
+        );
+        this.screwdriverKey = params.scene.input.keyboard.addKey(
+            Phaser.Input.Keyboard.KeyCodes.THREE
+        );
+        this.wrenchKey = params.scene.input.keyboard.addKey(
+            Phaser.Input.Keyboard.KeyCodes.FOUR
+        );
 
         // set up sound effects
         this.doorSound = this.scene.sound.add('door', {volume: 0.25});
@@ -71,6 +137,18 @@ export class Beetle extends Phaser.GameObjects.Sprite {
             this.beetleEvents.emit("enterDoor", "center");
         } else if (Phaser.Input.Keyboard.JustDown(this.actionKey)) {
             this.beetleEvents.emit("action");
+            if (this.toolEquipped === 1) {
+                this.anims.play('use-hammer', false);
+            }
+            if (this.toolEquipped === 2) {
+                this.anims.play('use-plunger', false);
+            }
+            if (this.toolEquipped === 3) {
+                this.anims.play('use-screwdriver', false);
+            }
+            if (this.toolEquipped === 4) {
+                this.anims.play('use-wrench', false);
+            }
         }
         else {
             this.handleMove();
@@ -116,6 +194,7 @@ export class Beetle extends Phaser.GameObjects.Sprite {
     }
 
     public stop(): void {
+        this.toolEquipped = 0;
         this.applyVelocity(0);
     }
 
@@ -128,7 +207,12 @@ export class Beetle extends Phaser.GameObjects.Sprite {
     }
 
     protected applyVelocity(velocity): void {
-        this.anims.play(velocity === 0 ? 'idle' : 'run', true);
+        if (velocity !== 0) {
+            this.anims.play('run', true);
+            this.toolEquipped = 0;
+        } else if (this.toolEquipped === 0) {
+            this.anims.play('idle');
+        }
         this.body.setVelocityX(velocity);
     }
 
@@ -144,6 +228,18 @@ export class Beetle extends Phaser.GameObjects.Sprite {
                 velocity = this.moveSpeed;
             }
             this.flipX = true;
+        } else if (this.hammerKey.isDown) {
+            this.toolEquipped = 1;
+            this.anims.play('hammer', false);
+        } else if (this.plungerKey.isDown) {
+            this.toolEquipped = 2;
+            this.anims.play('plunger', false);
+        } else if (this.screwdriverKey.isDown) {
+            this.toolEquipped = 3;
+            this.anims.play('screwdriver', false);
+        } else if (this.wrenchKey.isDown) {
+            this.toolEquipped = 4;
+            this.anims.play('wrench', false);
         }
         this.applyVelocity(velocity);
     }
